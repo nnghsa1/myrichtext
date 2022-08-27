1b900829308c96c92801853b1b9cb59bdd544eed
moxinghe <moxinghe@kingsoft.com>
2022-08-27 19:24:22 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import logo from './logo.svg';
+import styles from './App.less';
 
 function App() {
   return (
-    <div className="App">
-      hello
+    <div className={styles.App}>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
